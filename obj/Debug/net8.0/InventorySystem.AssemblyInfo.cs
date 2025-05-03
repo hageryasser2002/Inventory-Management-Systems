7305d6692c7c65de021e3d6c6d38be558284ac55
hageryasser2002 <hagerelkaood@gmail.com>
2025-05-03 21:54:57 +0300

    add

--- obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
+++ obj/Debug/net8.0/InventorySystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+92b95549593990429acde4c0accc5f53d60c1948")]
 [assembly: System.Reflection.AssemblyProductAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("InventorySystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
